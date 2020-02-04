c3400fae46d14fb343341c987e47eca109cc5cd4
ZhuoZhuoCrayon <873217631@qq.com>
2020-02-04 23:32:31 +0800

    搭建项目基本骨架

---
+++ src/test/java/com/crayon/youcanpass/YoucanPassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crayon.youcanpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoucanPassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
