63fdb7d45589e81c70f167d17df0c6c241120a60
ZhuoZhuoCrayon <873217631@qq.com>
2020-02-06 20:07:13 +0800

    添加参数校验注解

--- src/main/java/com/crayon/youcanpass/model/CmsCourse.java
+++ src/main/java/com/crayon/youcanpass/model/CmsCourse.java
@@ -1,6 +1,10 @@
 package com.crayon.youcanpass.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public class CmsCourse implements Serializable {
@@ -8,18 +12,27 @@ public class CmsCourse implements Serializable {
     private Long id;
 
     @ApiModelProperty(value = "开课单位id，精确到学院/学部")
+    @NotNull(message = "开课单位id不得为空")
+    @Min(value = 1, message = "开课id为正整数")
     private Long departmentId;
 
     @ApiModelProperty(value = "课程名称")
+    @NotNull(message = "课程名称不得为空")
     private String name;
 
     @ApiModelProperty(value = "课程类型:0->专业核心课/1->专业选修课/2-其他")
+    @NotNull(message = "课程类型不得为空")
+    @Range(min = 0, max = 3, message = "课程类型:0->专业核心课/1->专业选修课/2-其他")
     private Integer type;
 
     @ApiModelProperty(value = "开课学期:1->第一学期/2->第二学期/3->全年开设")
+    @NotNull(message = "开课学期不得为空")
+    @Range(min = 1, max = 3, message = "开课学期:1->第一学期/2->第二学期/3->全年开设")
     private Integer term;
 
     @ApiModelProperty(value = "启用状态:0->禁用/1->启用")
+    @NotNull(message = "启用状态不得为空")
+    @Range(min = 0, max = 1, message = "启用状态:0->禁用/1->启用")
     private Integer status;
 
     private static final long serialVersionUID = 1L;
