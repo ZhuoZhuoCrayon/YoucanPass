2faa834393e827606643123f5778e6845979511c
ZhuoZhuoCrayon <873217631@qq.com>
2020-02-12 00:26:52 +0800

    新增 [开课信息常量类]

---
+++ src/main/java/com/crayon/youcanpass/common/constant/CmsOpenCourseConstant.java
@@ -0,0 +1,15 @@
+package com.crayon.youcanpass.common.constant;
+
+/**
+ * @Classname CmsOpenCourseConstant
+ * @Description 开设课程常量设置类
+ * @Date 2020/2/10 22:54
+ * @Created by Crayon
+ */
+public class CmsOpenCourseConstant {
+    public static final int FAILED_AUDIT = -1;
+    public static final int WAITING_AUDIT = 0;
+    public static final int AUDITED = 1;
+    public static final int NOT_OPTIONAL = 2;
+    public static final int EXPIRED = 3;
+}
