e7e7d4eb6bc1c15013ca79917b4f9a677899e936
ZhuoZhuoCrayon <873217631@qq.com>
2020-02-12 23:09:33 +0800

    完善开课管理：加入批量删除/修改状态等接口

--- src/main/java/com/crayon/youcanpass/controller/CmsOpenCourseController.java
+++ src/main/java/com/crayon/youcanpass/controller/CmsOpenCourseController.java
@@ -8,12 +8,15 @@ import com.crayon.youcanpass.dto.CmsOpenCourseSimpleDto;
 import com.crayon.youcanpass.service.CmsOpenCourseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -33,7 +36,7 @@ public class CmsOpenCourseController {
 
     @ApiOperation("分页获取开课信息概要信息列表")
     @RequestMapping(value = "/list/simple", method = RequestMethod.GET)
-    //@PreAuthorize("hasAuthority('cms:opencourse:list:simple')")
+    @PreAuthorize("hasAuthority('cms:opencourse:list:simple')")
     public CommonResult<CommonPage<CmsOpenCourseSimpleDto>>
     listOpenCourseSimpleByQuery(CmsOpenCourseQuery openCourseQuery,
                                 @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
@@ -45,16 +48,81 @@ public class CmsOpenCourseController {
 
     @ApiOperation("新增开课信息：面向开课用户")
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    //@PreAuthorize("hasAuthority('cms:opencourse:insert')")
+    @PreAuthorize("hasAuthority('cms:opencourse:insert')")
     public CommonResult insertByLoginUser(@RequestBody @Valid CmsOpenCourseForUserParam openCourseForUserParam){
         int count = openCourseService.insertByLoginUser(openCourseForUserParam);
         CommonResult commonResult;
         if(count > 0){
             commonResult = CommonResult.success(count);
         }else{
-            commonResult = CommonResult.failed("更新失败");
+            commonResult = CommonResult.failed("新增开课信息失败");
         }
         return commonResult;
     }
 
+    @ApiOperation("删除开课信息")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('cms:opencourse:delete:id')")
+    public CommonResult delete(
+            @PathVariable("id")
+            @NotNull(message = "开课信息id不能为空")
+            @Min(value = 1, message = "id必须是正整数") Long id){
+        int count = openCourseService.delete(id);
+        CommonResult commonResult;
+        if(count > 0){
+            commonResult = CommonResult.success(count);
+        }else{
+            commonResult = CommonResult.failed("删除失败");
+        }
+        return commonResult;
+    }
+    @ApiOperation("批量删除开课信息")
+    @RequestMapping(value = "/delete/batch", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('cms:opencourse:delete:batch')")
+    public CommonResult deleteBatch(@RequestParam @NotNull(message = "课程id列表不能为空") List<Long> ids){
+        int count = openCourseService.deleteBatch(ids);
+        CommonResult commonResult;
+        if(count > 0){
+            commonResult = CommonResult.success(count);
+        }else{
+            commonResult = CommonResult.failed("批量删除失败");
+        }
+        return commonResult;
+    }
+
+    @ApiOperation("更新开课信息")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('cms:opencourse:update:id')")
+    public CommonResult update(
+            @PathVariable("id")
+            @NotNull(message = "开课信息id不能为空")
+            @Min(value = 1, message = "id必须是正整数") Long id,
+            @RequestBody @Valid CmsOpenCourseForUserParam openCourseForUserParam){
+        int count = openCourseService.update(id, openCourseForUserParam);
+        CommonResult commonResult;
+        if(count > 0){
+            commonResult = CommonResult.success(count);
+        }else{
+            commonResult = CommonResult.failed("更新开课信息失败");
+        }
+        return commonResult;
+    }
+
+    @ApiOperation("批量更新开课信息状态")
+    @RequestMapping(value = "/update/status/batch", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('cms:opencourse:update:status:batch')")
+    public CommonResult updateStatusBatch(
+            @RequestParam @NotNull(message = "开课信息id列表不能为空") List<Long> ids,
+            @RequestParam @NotNull(message = "开课状态不得为空")
+            @Range(min = 0, max = 1, message = "审核状态:-1->未通过审核/0->待审核/1->审核通过（可选）/2->不可选/3->过期不可选") Integer status,
+            @RequestParam String statusComment){
+        int count = openCourseService.updateStatusBatch(ids, status, statusComment);
+        CommonResult commonResult;
+        if(count > 0){
+            commonResult = CommonResult.success(count);
+        }else{
+            commonResult = CommonResult.failed("批量更新开课状态失败");
+        }
+        return commonResult;
+    }
 }
