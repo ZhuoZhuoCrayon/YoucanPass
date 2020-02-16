b8a79f82e38e42c986eaf20474d99a35de71cfe7
ZhuoZhuoCrayon <873217631@qq.com>
2020-02-16 22:37:30 +0800

    添加事务回滚注解

--- src/main/java/com/crayon/youcanpass/YoucanPassApiApplication.java
+++ src/main/java/com/crayon/youcanpass/YoucanPassApiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class YoucanPassApiApplication {
 
     public static void main(String[] args) {
